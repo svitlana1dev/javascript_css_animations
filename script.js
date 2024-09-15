e3ba6fac50829d2c4de69ee153cfd99934c6af54
svitlana1dev <ponomarchuk.svitlana.dev@gmail.com>
2024-09-16 00:48:57 +0300

    added cool glitch

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let video = document.querySelector(".video");
-let thumbs = document.querySelectorAll("ul li");
+let container = document.querySelector(".container");
 
-thumbs.forEach((thumb, i) => {
-  thumb.addEventListener("click", () => {
-    video.src = `videos/video${i + 1}.mp4`;
+for (let i = 0; i < 20; i++) {
+  let glitchBox = document.createElement("div");
+  glitchBox.classList.add("box");
+  container.appendChild(glitchBox);
+}
+
+setInterval(function () {
+  let glitches = document.querySelectorAll(".box");
+  glitches.forEach((glitch) => {
+    glitch.style.left = Math.random() * 100 + "vw";
+    glitch.style.top = Math.random() * 100 + "vw";
+    glitch.style.height = Math.random() * 100 + "px";
+    glitch.style.width = Math.random() * 400 + "px";
+    glitch.style.backgroundPosition = Math.random() * 50 + "px";
   });
-});
+}, 200);
